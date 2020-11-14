23e0d2d57dc7160709c38ffac9a6a41c60a0b429
MrSliddes <mrsliddes@outlook.com>
2020-11-15 00:59:49 +0100

    Finished day 3, allmost day 4 part 1

---
+++ HKU-Y2-PO/Assets/Scripts/Day 3/Day_3.cs
@@ -0,0 +1,216 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+
+public class Day_3 : MonoBehaviour
+{
+    //private Dictionary<Vector2, int> fabricGrid = new Dictionary<Vector2, int>();
+    private int[,] fabricGrid = new int[1000, 1000];
+    private List<int[]> cleanClaim = new List<int[]>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        CalcOverlap();
+
+        /*
+        string s = "#1 @ 16,576: 17x14";
+        s = s.Remove(0, 5);
+        print(s);
+        s = s.Replace(",", "-").Replace(":", "-").Replace(" ", "-").Replace("x", "-");
+        print(s);
+        int numberIndex = 0;
+        string x, y, width, height;
+        x = y = width = height = "";
+        bool isNumber = false;
+        foreach(char c in s)
+        {
+            if(c.Equals('-'))
+            {
+                if(isNumber == true)
+                {
+                    numberIndex++;
+                }
+                isNumber = false;
+                continue;
+            }
+            isNumber = true;
+
+            switch(numberIndex)
+            {
+                case 0:                    
+                    x += c;
+                    break;
+                case 1:
+                    y += c;
+                    break;
+                case 2:
+                    width += c;
+                    break;
+                case 3:
+                    height += c;
+                    break;
+                default: Debug.LogError("wrong");
+                    break;
+            }
+        }
+        print(int.Parse(x));
+        print(int.Parse(y));
+        print(int.Parse(width));
+        print(int.Parse(height));*/
+    }
+
+    private void CalcOverlap()
+    {
+        System.Diagnostics.Stopwatch t = new System.Diagnostics.Stopwatch();
+        t.Start();
+
+        // First read it from txt file
+        var sr = new StreamReader("Assets/Resources/day_3_puzzle_input.txt");
+        var fileContents = sr.ReadToEnd();
+        sr.Close();
+
+        var lines = fileContents.Split("\n"[0]);
+        print("Amount of lines: " + lines.Length);
+
+        // Convert string into int array
+        string[] lineArray = new string[lines.Length];
+
+        for(int i = 0; i < lines.Length; i++)
+        {
+            // Convert string to numbers
+            // first number starts at char 6
+            lineArray[i] = string.Concat(lines[i].Where(c => !char.IsWhiteSpace(c)));
+            string s = lines[i];
+            // Remove junk from string
+            int atIndex = s.IndexOf("@");
+            // Get id int
+            int id = int.Parse(s.Substring(1, atIndex - 2));
+            s = s.Remove(0, atIndex+1); // Can remove first 6 chars, but as #number increases it takes longer
+            s = s.Replace(",", "-").Replace(":", "-").Replace(" ", "-").Replace("x", "-");
+            int numberIndex = 0;
+            string x, y, width, height;
+            x = y = width = height = "0";
+            bool isNumber = false;
+            //print(s);
+            foreach(char c in s)
+            {
+                if(c.Equals('-'))
+                {
+                    if(isNumber == true)
+                    {
+                        numberIndex++;
+                    }
+                    isNumber = false;
+                    continue;
+                }
+                isNumber = true;
+
+                switch(numberIndex)
+                {
+                    case 0:
+                        x += c;
+                        break;
+                    case 1:
+                        y += c;
+                        break;
+                    case 2:
+                        width += c;
+                        break;
+                    case 3:
+                        height += c;
+                        break;
+                    default:
+                        Debug.LogError("wrong");
+                        break;
+                }
+            }
+
+            //print(x);
+            //print(y);
+            //print(width);
+            //print(height);
+            //print(int.Parse(x));
+            //print(int.Parse(y));
+            //print(int.Parse(width));
+            //print(int.Parse(height));
+            PositionClaim(int.Parse(x), int.Parse(y), int.Parse(width), int.Parse(height), id);
+        }
+
+        // Loop trough grid and get inches coverd
+        int coverdInchesSquare = 0;
+        for(int x = 0; x < fabricGrid.GetLength(0); x++)
+        {
+            for(int y = 0; y < fabricGrid.GetLength(1); y++)
+            {
+                if(fabricGrid[x, y] > 1) coverdInchesSquare++;
+            }
+        }
+        print("Coverd inches in square: " + coverdInchesSquare);
+
+        t.Stop();
+        Debug.Log("(Part 1) This spagetti was cooked in: " + t.ElapsedMilliseconds + "ms, or " + t.ElapsedMilliseconds / 1000 + " seconds.");
+        t.Start();
+
+        // Dont loop trough a million grid, takes too long
+        // Loop trough cleanClaim positions and check if they are still clean
+        print("Claims that seem clean: " + cleanClaim.Count);
+        for(int i = 0; i < cleanClaim.Count; i++)
+        {
+            bool stillClean = true;
+            for(int x = cleanClaim[i][0]; x < cleanClaim[i][0] + cleanClaim[i][2]; x++)
+            {
+                for(int y = cleanClaim[i][1]; y < cleanClaim[i][1] + cleanClaim[i][3]; y++)
+                {
+                    if(fabricGrid[x, y] > 1) stillClean = false;
+                }
+            }
+            if(stillClean)
+            {
+                Debug.Log("Claim that doesnt overlap is #" + cleanClaim[i][4]);
+                break;
+            }
+        }
+
+        t.Stop();
+        Debug.Log("(Part 2) This spagetti was cooked in: " + t.ElapsedMilliseconds + "ms, or " + t.ElapsedMilliseconds / 1000 + " seconds.");
+    }
+
+    private void PositionClaim(int posX, int posY, int width, int height, int iD)
+    {
+        // Add claim to array
+        bool claimWasUnclaimed = true;
+        for(int x = posX; x < posX + width; x++)
+        {
+            for(int y = posY; y < posY + height; y++)
+            {
+                if(fabricGrid[x, y] > 0) claimWasUnclaimed = false;
+                fabricGrid[x, y]++;
+            }
+        }
+
+        // P2
+        if(claimWasUnclaimed)
+        {
+            cleanClaim.Add(new int[] { posX, posY, width, height, iD});
+        }
+    }
+}
+
+
+/* Analyze of day 2 
+ * 
+ * Looks like a hard assignment. See u in 2 hours
+ * 
+ * Went faster than expected. First code does it in 171ms (part 1) awnser was 111630 square inches
+ * Now part 2
+ * 
+ * it is not fast to loop trough a million grid.
+ * Hour later and we got it! awnser was id #724. completed it in 34ms
+ * Optimized part 1 (removed junk) and that runs in 31ms
+ 
+     
+     */
---
+++ HKU-Y2-PO/Assets/Scripts/Day 4/Day_4.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.IO;
+
+public class Day_4 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SortGuards();
+    }
+
+    private void SortGuards()
+    {
+        System.Diagnostics.Stopwatch t = new System.Diagnostics.Stopwatch();
+        t.Start();
+
+        // First read it from txt file
+        var sr = new StreamReader("Assets/Resources/day_4_puzzle_input.txt");
+        var fileContents = sr.ReadToEnd();
+        sr.Close();
+
+        var lines = fileContents.Split("\n"[0]);
+        print("Amount of lines: " + lines.Length);
+
+        // Convert string into int array
+        string[] lineArray = new string[lines.Length];
+               
+        // Initialize lineArray
+        for(int i = 0; i < lines.Length; i++)
+        {
+            lineArray[i] = string.Concat(lines[i].Where(c => !char.IsWhiteSpace(c)));            
+        }
+
+        // Sort array only on date format
+        System.Array.Sort(lineArray, delegate (string twt1, string twt2)
+        {
+            if(twt1 == null && twt2 == null) return 0;
+            if(twt1 == null) return -1;
+            if(twt2 == null) return 1;
+            return twt1.Substring(0, 16).CompareTo(twt2.Substring(0, 16));
+        });
+
+        // Test feedback
+        for(int i = 0; i < lineArray.Length; i++)
+        {
+            //print(lineArray[i]);
+        }
+
+        // Now make list of guard id followed by minutes sleeped
+        int currentGuardIdList = 0;
+        List<Vector2> guards = new List<Vector2>(); // x = id, y = timeasleepminutes
+        for(int i = 0; i < lineArray.Length; i++)
+        {
+            string s = lineArray[i];
+            s = s.ToLower();
+            int startTimeFallsAsleep = 0;
+            // Check for guard difference
+            if(s.IndexOf("guard") != -1)
+            {
+                // new guard                
+                guards.Add(new Vector2(GetGuardIDFromString(s), 0));
+                currentGuardIdList = guards.Count - 1;
+            }
+            else
+            {
+                // same guard, get action
+                if(s.IndexOf("falls") != -1)
+                {
+                    // Get the start minute
+                    string time = s.Substring(s.IndexOf("]") - 2, 2);
+                    startTimeFallsAsleep = int.Parse(time);
+                }
+                else if(s.IndexOf("wakes") != -1)
+                {
+                    // add time to start minute
+                    string time = s.Substring(s.IndexOf("]") - 2, 2);
+                    guards[currentGuardIdList] = new Vector2(guards[currentGuardIdList].x, guards[currentGuardIdList].y + (int.Parse(time) - startTimeFallsAsleep));
+                }
+            }
+        }
+
+        // Show guards (debug)
+        for(int i = 0; i < guards.Count; i++)
+        {
+            print("guard: " + guards[i]);
+        }
+
+        // Get the heighest min asleep guard
+        int heighest = 0;
+        for(int i = 0; i < guards.Count; i++)
+        {
+            if(guards[i].y > guards[heighest].y) heighest = i;
+        }
+
+        Debug.Log("The guard with id: " + guards[heighest].x + " has the heighest sleep in minutes: " + guards[heighest].y + ". That makes part 1 awnser: " + guards[heighest].x * guards[heighest].y);
+
+        t.Stop();
+        Debug.Log("(Part 1) This spagetti was cooked in: " + t.ElapsedMilliseconds + "ms, or " + t.ElapsedMilliseconds / 1000 + " seconds.");
+    }
+
+    private int GetGuardIDFromString(string s)
+    {
+        // String comes in, check for # then continue in string and check if char is Parsable
+        int startIndex = s.IndexOf("#");
+        string id = "";
+        int result;
+        for(int i = startIndex + 1; i < s.Length; i++)
+        {
+            if(int.TryParse(s[i].ToString(), out result))
+            {
+                id += s[i];
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return int.Parse(id);
+    }
+}
+
+/* Day 4 analyze
+ * Gotta sort that stuff first
+ * Got awnser 123083, that was wrong, first time i got it wrong when trying the full code.
+ * Thatss because i read the question wrong... I need to find the minute the guard is most asleep on. not just id * allminutessleeped
+ * Thats a job for future me cause it is 00:58 right now and i wanna sleep. 
+ */
